8e58135713c81b5fb6a3094b2bfa8b3b83dd59d1
qin2007 <qin@ractoc.com>
2025-02-22 15:45:27 +0100

    gitignore

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import './createPost.js';
+
+import { Devvit, useWebView } from '@devvit/public-api';
+
+Devvit.configure({
+  redditAPI: true,
+  redis: true,
+});
+
+// Add a custom post type to Devvit
+Devvit.addCustomPostType({
+  name: 'Web View Example',
+  height: 'tall',
+  render: (context) => {
+    // Load username with `useAsync` hook
+
+    const webView = useWebView({
+      // URL of your web view content
+      url: 'page.html',
+
+      // Handle messages sent from the web view
+      async onMessage(message, webView) { },
+      onUnmount() {
+        context.ui.showToast('Web view closed!');
+      },
+    });
+
+    // Render the custom post type
+    return (
+      <vstack grow padding="small">
+        <button onPress={() => webView.mount()}>Launch App</button>
+      </vstack>
+    );
+  },
+});
+
+export default Devvit;
